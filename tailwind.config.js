45716ba1a15b321935b4ebc55814f01bb32e9c3d
rileytoddherman <rileyherman@tuta.io>
2021-05-14 23:32:57 -0600

    editorconfig and some formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,28 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false,
   theme: {
     extend: {},
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
+      transparent: "transparent",
+      current: "currentColor",
       black: colors.black,
       white: colors.white,
       gray: colors.trueGray,
       teal: {
-        light: '#D1FAE5',
-        DEFAULT: '#34D399',
-        dark: '#047857'
+        light: "#D1FAE5",
+        DEFAULT: "#34D399",
+        dark: "#047857",
       },
       amber: {
-        light: '#FEF3C7',
-        DEFAULT: '#FBBF24',
-        dark: '#B45309'
-      }
-    }
+        light: "#FEF3C7",
+        DEFAULT: "#FBBF24",
+        dark: "#B45309",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
