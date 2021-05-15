d16623d617a7334e3af2fed4b728452ce577d2db
rileytoddherman <rileyherman@tuta.io>
2021-05-14 23:23:44 -0600

    lots of initial shell stuff

---
+++ pages/login.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+  <div className="h-full flex flex-col items-center justify-center">
+    <input type="text" className="mb-2 w-64 border"></input>
+    <input type="password" className="mb-2 w-64 border"></input>
+  </div>
+  )
+}
