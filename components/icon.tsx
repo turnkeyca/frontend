fb402c1bc905f696a1f4c353a640ae41bb6b24f4
rileytoddherman <rileyherman@tuta.io>
2021-06-23 16:53:28 -0600

    reference page

--- components/icon.tsx
+++ components/icon.tsx
@@ -4,23 +4,30 @@ export interface IIcon extends HTMLAttributes<HTMLElement> {
   name: "chevron_right" | "error_outline" | "arrow_back" | "edit" | "logout";
   handleClick?: () => any;
   small?: boolean;
+  className?: string;
 }
-export const Icon = ({ name, handleClick, small, ...rest }: IIcon) => {
+export const Icon = ({
+  name,
+  handleClick,
+  small,
+  className,
+  ...rest
+}: IIcon) => {
   let iconClasses = "material-icons";
-  let containerClasses = "";
+  let containerClasses = className ? className : "";
   if (small) {
     iconClasses += " md-18";
-    containerClasses += "h-4";
+    containerClasses += " h-4";
   } else {
     iconClasses += " md-24";
-    containerClasses += "h-6";
+    containerClasses += " h-6";
   }
   if (handleClick) {
     containerClasses += " cursor-pointer";
   }
 
   return (
-    <div className={containerClasses}>
+    <div className={containerClasses} onClick={handleClick}>
       <span className={iconClasses}>{name}</span>
     </div>
   );
--- pages/renter/reference/index.tsx
+++ pages/renter/reference/index.tsx
@@ -1,2 +1,59 @@
-import React from "react"
+import React from "react";
+import { Button } from "../../../components/button";
+import { Footer } from "../../../components/footer";
+import { Header } from "../../../components/header";
+import { Icon } from "../../../components/icon";
+import { Picture } from "../../../components/image";
 
+export default function Reference(): any {
+  return (
+    <div>
+      <Header
+        title="My Profile"
+        showEdit={true}
+        showBack={true}
+        showLogout={false}
+      />
+      <div className="p-3">
+        <div className="flex items-center justify-center">
+          <span className="tk-text-blue font-medium text-xl p-3">
+            Reference Info
+          </span>
+        </div>
+        <div className="grid grid-cols-1 gap-1 mb-3">
+          <div className="border rounded p-1 shadow">
+            <div className="grid grid-cols-3 gap-2 mb-2 tracking-wide">
+              <div className="flex flex-col items-center">
+                <Picture
+                  alt="profile picture"
+                  src="../../public/favicon-32x32.png"
+                />
+              </div>
+              <div className="col-span-2 w-full flex ">
+                <div>
+                  <div className="tk-text-blue text-lg font-medium">
+                    Brooke Ham
+                  </div>
+                  <div className="tk-text-blue">brooke@ham.ca</div>
+                  <div className="tk-text-blue">123-456-7890</div>
+                  <div className="text-gray-600 text-sm">
+                    Don't call in the mornings - she gets cranky.
+                  </div>
+                </div>
+                <Icon
+                  className="text-gray-700"
+                  handleClick={() => alert("click")}
+                  name="edit"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <Button variant="tertiary">Add new reference</Button>
+        </div>
+      </div>
+      <Footer showProfile={true} showShareMyProfile={true} />
+    </div>
+  );
+}
