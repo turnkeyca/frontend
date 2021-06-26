fde4f2d79ac10470f3700fae24889ee2ea979a8c
rileytoddherman <rileyherman@tuta.io>
2021-06-26 17:19:00 -0600

    button toggle

--- components/binarytoggle.tsx
+++ components/binarytoggle.tsx
@@ -1,26 +1,60 @@
-import React, { HTMLAttributes } from "react";
-import { Button } from "./button";
+import React, { HTMLAttributes, useState } from "react";
 
 export interface IToggle extends HTMLAttributes<HTMLElement> {
   className?: string;
   labelTrue: string;
   labelFalse: string;
+  initialValue?: boolean;
 }
+
+let TRUE_CLASSES_TRUE = "tk-bg-teal text-white";
+let TRUE_CLASSES_FALSE = "bg-white tk-text-teal";
+let FALSE_CLASSES_TRUE = "bg-white tk-text-amber";
+let FALSE_CLASSES_FALSE = "tk-bg-amber text-white";
 
 export const Toggle = ({
   className,
   labelTrue,
   labelFalse,
+  initialValue,
   ...rest
 }: IToggle) => {
+  let [value, setState] = useState(initialValue ? initialValue : false);
+  const toggleValue = (incomingValue: boolean) => {
+    if (value) {
+      if (incomingValue) {
+        setState(false);
+      } else {
+        setState(true);
+      }
+    } else {
+      if (incomingValue) {
+        setState(true);
+      } else {
+        setState(false);
+      }
+    }
+  };
   return (
-    <div className={"flex ".concat(className)}>
-      <Button className="mr-1">
-        <span>{labelTrue}</span>
-      </Button>
-      <Button variant="secondary">
-        <span>{labelFalse}</span>
-      </Button>
+    <div className={"flex w-full ".concat(className)}>
+      <div
+        id="true"
+        onClick={() => toggleValue(true)}
+        className={"flex justify-center items-center w-full cursor-pointer border tk-border-teal rounded p-3 mr-1 ".concat(
+          value ? TRUE_CLASSES_TRUE : TRUE_CLASSES_FALSE
+        )}
+      >
+        <span className="tracking-wide font-medium text-sm">{labelTrue}</span>
+      </div>
+      <div
+        id="false"
+        onClick={() => toggleValue(true)}
+        className={"flex justify-center items-center w-full cursor-pointer border tk-border-amber rounded p-3 ".concat(
+          value ? FALSE_CLASSES_TRUE : FALSE_CLASSES_FALSE
+        )}
+      >
+        <span className="tracking-wide font-medium text-sm">{labelFalse}</span>
+      </div>
     </div>
   );
 };
