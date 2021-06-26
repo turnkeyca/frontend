b14b1f6dec56e08a3814d00ade7613a5d246d65c
rileytoddherman <rileyherman@tuta.io>
2021-06-26 12:36:23 -0600

    edit renter

--- pages/renter/edit.tsx
+++ pages/renter/edit.tsx
@@ -1,20 +1,60 @@
+import { useRouter } from "next/router";
 import React from "react";
+import { Button } from "../../components/button";
 import { Footer } from "../../components/footer";
 import { Header } from "../../components/header";
 
-export default class Renter extends React.Component {
-  render(): any {
-    return (
-      <div>
-        <Header
-          showEdit={false}
-          showBack={true}
-          showLogout={false}
-          title="Edit Profile"
-        />
-        <div className="p-3"></div>
-        <Footer showProfile={false} showConnections={false} />
+export default function EditRenter() {
+  const router = useRouter();
+  return (
+    <div>
+      <Header
+        showEdit={false}
+        showBack={true}
+        showLogout={false}
+        title="Edit Profile"
+      />
+      <div className="p-3">
+        <div className="grid grid-cols-1 gap-1">
+          <Button
+            handleClick={() => router.push("/renter/contact/edit")}
+            variant="tertiary"
+          >
+            Contact information
+          </Button>
+          <Button
+            handleClick={() => router.push("/renter/employment/edit")}
+            variant="tertiary"
+          >
+            Employment information
+          </Button>
+          <Button
+            handleClick={() => router.push("/renter/lease/edit")}
+            variant="tertiary"
+          >
+            Lease information
+          </Button>
+          <Button
+            handleClick={() => router.push("/renter/general/edit")}
+            variant="tertiary"
+          >
+            General information
+          </Button>
+          <Button
+            handleClick={() => router.push("/renter/reference/edit")}
+            variant="tertiary"
+          >
+            Reference information
+          </Button>
+          <Button
+            handleClick={() => router.push("/account/edit")}
+            variant="tertiary"
+          >
+            Change password
+          </Button>
+        </div>
       </div>
-    );
-  }
+      <Footer showProfile={false} showConnections={false} />
+    </div>
+  );
 }
