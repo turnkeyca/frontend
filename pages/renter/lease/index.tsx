a248dba8c26b6504bcc7267c90a382a1d614dc8e
rileytoddherman <rileyherman@tuta.io>
2021-07-17 17:30:32 -0600

    lease stuff

--- pages/renter/lease/index.tsx
+++ pages/renter/lease/index.tsx
@@ -1,68 +1,110 @@
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { Button } from "../../../components/button";
 import { Footer } from "../../../components/footer";
 import { Header } from "../../../components/header";
+import { Error } from "../../../components/error";
+import { UserApi } from "../../../generated-src/openapi";
 
-export default class Lease extends React.Component {
-  render(): any {
-    return (
-      <div>
-        <Header
-          title="My Profile"
-          showEdit={true}
-          showBack={true}
-          showLogout={false}
-        />
-        <div className="p-3">
-          <div className="flex items-center justify-center border border-t-0 border-l-0 border-r-0">
-            <span className="tk-text-blue font-medium text-xl p-3">
-              Lease Info
+export default function General() {
+  const router = useRouter();
+  let [[error, user, userId], setState] = useState([
+    undefined,
+    undefined,
+    undefined,
+  ]);
+  useEffect(() => {
+    if (!router.isReady) {
+      return;
+    }
+    let _userId = router.query.userId as string;
+    const userApi = new UserApi();
+    const sub = userApi.getUser({ id: _userId }).subscribe({
+      next: (u) => setState([undefined, u, _userId]),
+      error: (e) => setState([e, undefined, _userId]),
+    });
+    return () => sub.unsubscribe();
+  }, [router.isReady]);
+  return (
+    <div>
+      <Header
+        title="My Profile"
+        showEdit={true}
+        showBack={true}
+        showLogout={false}
+      />
+      <div className="p-3">
+        {!!error && <Error error={error} />}
+        <div className="flex items-center justify-center border border-t-0 border-l-0 border-r-0">
+          <span className="tk-text-blue font-medium text-xl p-3">
+            Lease Info
+          </span>
+        </div>
+        <div className="grid grid-cols-1">
+          <div className="grid grid-cols-1 gap-1 border border-t-0 border-l-0 border-r-0 p-3">
+            <span className="tk-text-blue tracking-wide">
+              Why are you looking for a place to live?
+            </span>
+            <span className="text-gray-600 text-sm tracking-wide">
+              {user?.movingReason}
             </span>
           </div>
-          <div className="grid grid-cols-1">
-            <div className="grid grid-cols-1 gap-1 border border-t-0 border-l-0 border-r-0 p-3">
-              <span className="tk-text-blue tracking-wide">
-                Why are you looking for a place to live?
-              </span>
-              <span className="text-gray-600 text-sm tracking-wide">
-                Because my parents threw me out
-              </span>
-            </div>
-            <div className="grid grid-cols-1 gap-1 border border-t-0 border-l-0 border-r-0 p-3">
-              <span className="tk-text-blue tracking-wide">
-                Will you be living with anyone else?
-              </span>
-              <span className="text-gray-600 text-sm tracking-wide">No</span>
-            </div>
-            <div className="grid grid-cols-1 gap-1 border border-t-0 border-l-0 border-r-0 p-3">
-              <span className="tk-text-blue tracking-wide">
-                When is your preferred move in date?
-              </span>
-              <span className="text-gray-600 text-sm tracking-wide">
-                tomorrow
-              </span>
-            </div>
-            <div className="grid grid-cols-1 gap-1 border border-t-0 border-l-0 border-r-0 p-3">
-              <span className="tk-text-blue tracking-wide">
-                Are you able to pay the security deposit with your first month's
-                rent?
-              </span>
-              <span className="text-gray-600 text-sm tracking-wide">Yes</span>
-            </div>
-            <div className="grid grid-cols-1 gap-1 border border-t-0 border-l-0 border-r-0 p-3">
-              <span className="tk-text-blue tracking-wide">
-                Additional information
-              </span>
-              <span className="text-gray-600 text-sm tracking-wide">
-                Well this one time out at the farm my buddies and I weeeehooo
-              </span>
+          <div className="grid grid-cols-1 gap-1 border border-t-0 border-l-0 border-r-0 p-3">
+            <span className="tk-text-blue tracking-wide">
+              Have you ever been party to a lawsuit?
+            </span>
+            <span className="text-gray-600 text-sm tracking-wide">
+              {user?.lawsuit}
+            </span>
+          </div>
+          <div className="grid grid-cols-1 gap-1 border border-t-0 border-l-0 border-r-0 p-3">
+            <span className="tk-text-blue tracking-wide">
+              Will you be living with anyone?
+            </span>
+            <span className="text-gray-600 text-sm tracking-wide">
+              {user?.roommates}
+            </span>
+            <div>
+              <Button
+                handleClick={() =>
+                  router.push({
+                    pathname: "/renter/roommate",
+                    query: { userId },
+                  })
+                }
+                variant="secondary"
+              >
+                View roommate info
+              </Button>
             </div>
           </div>
+          <div className="grid grid-cols-1 gap-1 border border-t-0 border-l-0 border-r-0 p-3">
+            <span className="tk-text-blue tracking-wide">
+              Would you be willing to do a credit check?
+            </span>
+            <span className="text-gray-600 text-sm tracking-wide">
+              {user?.creditCheck}
+            </span>
+          </div>
+          <div className="grid grid-cols-1 gap-1 border border-t-0 border-l-0 border-r-0 p-3">
+            <span className="tk-text-blue tracking-wide">
+              Will you be able to pay the security deposit?
+            </span>
+            <span className="text-gray-600 text-sm tracking-wide mb-1">
+              {user?.securityDeposit}
+            </span>
+          </div>
+          <div className="grid grid-cols-1 gap-1 border border-t-0 border-l-0 border-r-0 p-3">
+            <span className="tk-text-blue tracking-wide">
+              Additional information
+            </span>
+            <span className="text-gray-600 text-sm tracking-wide">
+              {user?.additionalDetailsLease}
+            </span>
+          </div>
         </div>
-        <Footer
-          showProfile={true}
-          showConnections={true}
-        />
       </div>
-    );
-  }
+      <Footer showProfile={true} showConnections={true} />
+    </div>
+  );
 }
