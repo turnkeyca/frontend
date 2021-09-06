d645180de0a45efd2e19dd68a444abb01ae69b44
rileytoddherman <rileyherman@tuta.io>
2021-09-06 11:44:41 -0600

    fix rewrites by doing redirects

--- next.config.js
+++ next.config.js
@@ -2,20 +2,15 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: "/renter/contact",
-        destination: "/renter/contact/edit",
+        source: `/renter/contact`,
+        destination: `/renter/contact/edit`,
         permanent: false,
       },
+      {
+        source: `/v1/:path*`,
+        destination: `${process.env.API_URI}/v1/:path*`,
+        permanent: true,
+      },
     ];
-  },
-  async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: `/v1/:path*`,
-          destination: `${process.env.API_URI}/v1/:path*`,
-        },
-      ]
-    };
   },
 };
