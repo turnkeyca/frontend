e509c2e584c8c1e92975248a67bc28ad258fb81b
mab839 <mab839@usask.ca>
2022-02-05 17:36:29 -0600

    began building out the walkthrough pages. The header styles doesn't seem to be displaying

---
+++ pages/renter/walkthrough/1/index.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { UserApi } from "../../../../generated-src/openapi";
+import { useRouter } from "next/router";
+import { Error, Footer, Header, Icon, ProgressBar } from "../../../../components";
+import { WalkProgress } from "../../../../components/walkthroughprogressbar";
+
+export default function RenterWalkthrough1() {
+    const router = useRouter();
+    let [[error, user, userId], setState] = useState([
+        undefined,
+        undefined,
+        undefined,
+    ]);
+    useEffect(() => {
+        if (!router.isReady) {
+            return;
+        }
+        let _userId = router.query.userId as string;
+        const userApi = new UserApi();
+        const sub = userApi
+            .getUser({ id: _userId, token: router.query.token as string })
+            .subscribe({
+                next: (u) => setState([undefined, u, _userId]),
+                error: (e) => setState([e, undefined, _userId]),
+            });
+        return () => sub.unsubscribe();
+    }, [router.isReady, router.query]);
+    return (
+        <div>
+            <Header
+                router={router}
+                title="My Turnkey"
+                showEdit={true}
+                showBack={false}
+                showLogout={true}
+            />
+            <WalkProgress progress="15%"/>
+        </div>
+    );
+}
