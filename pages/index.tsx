a35e6f16d155ebbb78e3d538d612d30a3c0a0a80
mab839 <mab839@usask.ca>
2022-02-06 15:07:54 -0600

    Added a login/signup welcom page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,25 @@
 import React from "react";
+import { useRouter } from "next/router";
+import { Button } from "../components";
 
 export default function Index() {
-  return <></>
+  const router = useRouter();
+  return (
+    <div className="tk-bg-teal place-items-center h-screen w-screen">
+      <p className="text-center tk-text-blue text-medium p-8">Simplifying the rental application process</p>
+      <div className="flex flex-col gap-5 px-16">
+        <Button variant="secondary" handleClick={() =>
+          router.push({
+            pathname: "/signup"
+          })}>
+          Sign up
+        </Button>
+        <Button variant="tertiary" handleClick={() =>
+          router.push({
+            pathname: "/login"
+          })}>
+          Log In
+        </Button>
+      </div>
+    </div>)
 }
