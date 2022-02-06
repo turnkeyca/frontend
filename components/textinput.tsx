fe93b27fa40d58aa8caaa0f3a9e652984d593619
mab839 <mab839@usask.ca>
2022-02-06 16:22:01 -0600

    Added a text input component

---
+++ components/textinput.tsx
@@ -0,0 +1,15 @@
+import React, { HTMLAttributes } from "react";
+
+export interface ITextField extends HTMLAttributes<HTMLElement> {
+    type?: "text" | "email" | "password";
+}
+
+export const TextField = ({
+    children,
+    type = "text",
+    ...rest
+}:ITextField) => {
+    return (
+          <input type={type} className="tk-border-blue border-2 rounded-xl p-2 w-full"></input>
+      )
+}
